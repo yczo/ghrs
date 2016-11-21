b360d444efd79acf64d1f11612c1855da0b9728d
Zou <duekicrcvo@163.com>
2016-11-21 14:31:16 +0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ghrs
+
+See the size of a github repo before cloning it
+
+Python 3.5.2
+
+## How TO
+
+```bash
+$ python ghrs.py -o owner -r repo
+```
+
+## Example
+
+```bash
+# https://github.com/apple/swift
+
+$ python ghrs.py -o apple -r swift
+```
--- ghrs.py
+++ ghrs.py
@@ -6,17 +6,17 @@
 import sys, getopt
 import requests
 
-opts, args = getopt.getopt(sys.argv[1:], "u:r:")
-user = ""
+opts, args = getopt.getopt(sys.argv[1:], "o:r:")
+owner = ""
 repo = ""
 
 for op, value in opts:
-	if op == "-u":
-		user = value
+	if op == "-o":
+		owner = value
 	elif op == "-r":
 		repo = value
 
-r = requests.get('https://api.github.com/repos/' + user + '/' + repo)
+r = requests.get('https://api.github.com/repos/' + owner + '/' + repo)
 if r.status_code == 200:
 	print(r.json()['size'])
 
